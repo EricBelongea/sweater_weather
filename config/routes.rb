fe775dea819229c4f07cdbaf3dd9a3cc2c68203b
Eric Belongea <eric.belongea@gmail.com>
2024-01-13 15:42:00 -0700

    Search renders the json that we expect; starting testing

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  namespace :api do
-    namespace :v0 do
-      resources :forecasts, only: :index 
-      get '/forecast', to: 'forecasts#search'
-    end
-  end
-end
