74c20a3a0f8f20b04b98a813e9024342beae9623
Eric Belongea <eric.belongea@gmail.com>
2024-01-13 17:16:50 -0700

    Adds password to users

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,6 +1,19 @@
 class Api::V0::UsersController < ApplicationController
   def create
+    # require 'pry'; binding.pry
+    user = User.new(user_params)
     require 'pry'; binding.pry
 
+    if user.save 
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: { error: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
-class User
-  validates :email, :api_key, presence: true
-  validates :email, :api_key, uniqueness: true
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+class User < ApplicationRecord
+  
+  validates :email, presence: true
+  # validates :email, uniqueness: true
+  # validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates_presence_of :password_digest
+  # before_create :generate_api_key
 
   has_secure_password
+
+  private
+
+  # def generate_api_key
+  #   self.api_key = SecureRandom.uuid
+  # end
 end
