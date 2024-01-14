2411d0f77c7e13298e5ad5b0db69de5561e753a0
Eric Belongea <eric.belongea@gmail.com>
2024-01-13 18:34:33 -0700

    Full sad pathing for user creation

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -5,13 +5,13 @@ class Api::V0::UsersController < ApplicationController
     if user.save
       render json: UserSerializer.new(user), status: :created
     else
-      render json: { error: user.errors.full_messages }, status: :bad_request
+      render json: { error: user.errors.full_messages.join(', ') }, status: :bad_request
     end
   end
 
   private
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
