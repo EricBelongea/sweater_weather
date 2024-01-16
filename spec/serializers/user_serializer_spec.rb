043a8ab2631aa1887fde0a622408d9efaeef885d
Eric Belongea <eric.belongea@gmail.com>
2024-01-16 11:54:20 -0700

    Building out testing

--- spec/serializers/user_serializer_spec.rb
+++ spec/serializers/user_serializer_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "User Serializer" do
-  xit "Can create parse into JSON" do
-    user = create(:user)
-    require 'pry'; binding.pry
+  it "Can create parse into JSON" do
+    user1 = User.create(email: "example#gemail.com", password_digest: "123abc", api_key: "123456789")
+
+    expect(user1).to be_instance_of(User)
   end
 end
